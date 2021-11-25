ef3301bee37221458bca494c28448843b1563441
Mat <mathewharvey@gmail.com>
2021-11-25 17:22:22 +0800

    webcam screenshot working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 import OemPlayer from './oemPlayer';
 import ScreenCapture from './screenCapture';
-import InputSrc from './inputSrc';
+
 import WebCamVideo from './webCamVideo';
 
 
 
 function App() {
+  const [showOem, setShowOEM] = useState(true)
+    const [showWebCam, setShowWebCam] = useState(true)
   return (
     <>
-      <InputSrc />
-      <OemPlayer />
-      <WebCamVideo />
+     {
+                showOem ? 
+                <p><OemPlayer /></p>
+                : null
+            }
+            {
+                showWebCam ? 
+              <p><WebCamVideo /></p>
+                : null
+            }
+
+            <button id="inputSrc" onClick={() => setShowOEM(!showOem)}> External Camera toggle</button>
+            <button id="inputSrc" onClick={() => setShowWebCam(!showWebCam)}> Webcam toggle</button>
+      
+      
       <ScreenCapture />
 
     </>
--- src/webCamVideo.js
+++ src/webCamVideo.js
@@ -2,12 +2,10 @@ import React, { useEffect, useRef, useState } from 'react';
 
 export default function WebCamVideo() {
     const videoRef = useRef(null);
+    const photoRef = useRef(null);
+    const [hasPhoto, setHasPhoto] = useState(false)
 
-    useEffect(() => {
-      getVideo();
-    }, [videoRef]);
-  
-    const getVideo = () => {
+    const getWebCamVideo = () => {
       navigator.mediaDevices
         .getUserMedia({ video: { width: 300 } })
         .then(stream => {
@@ -19,12 +17,45 @@ export default function WebCamVideo() {
           console.error("error:", err);
         });
     };
+
+    const takePhoto = () => {
+        const width = 414
+        const height = width / (16/9);
+
+        let video = videoRef.current;
+        let photo = photoRef.current;
+
+        photo.width = width;
+        photo.heigh = height;
+
+        let ctx = photo.getContext('2d');
+        ctx.drawImage(video, 0, 0,width, height);
+        setHasPhoto(true);
+    }
+
+    const closePhoto = () => {
+        let photo = photoRef.current
+        let ctx = photo.getContext('2d');
+         ctx.clearRect(0, 0, photo.width, photo.height)
+        setHasPhoto(false);
+    }
+
+    useEffect(() => {
+        getWebCamVideo();
+      }, [videoRef]);
+
     return (
         <div> 
-          <div title = "Device Camera">
+          <div id = "webCamVideo">
       
             <video ref={videoRef} />
+            <button onClick={takePhoto}>Screenshot</button>
           </div>
+          <div className={"result" + (hasPhoto ? "hasPhoto" : " ")}>
+              <canvas ref={photoRef}></canvas>
+              <button onClick={closePhoto}>Close</button>
+          </div>
+
         </div>
       );
 }
